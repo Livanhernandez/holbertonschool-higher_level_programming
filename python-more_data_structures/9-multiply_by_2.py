71cac598aae11169c8ae1cf222be466d51a6e687
Livanhernandez <livanhernandez9@gmail.com>
2024-01-30 14:35:52 -0400

    key*2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dic = {}
     if a_dictionary:
         for key in a_dictionary:
-            a_dictionary[key] = a_dictionary[key] * 2
-    return a_dictionary
+            new_dic[key] = a_dictionary[key] * 2
+    return new_dic
