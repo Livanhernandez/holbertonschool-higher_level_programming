77997cd3b21b338d3735cd9da125199656e42e24
Livanhernandez <livanhernandez9@gmail.com>
2024-02-02 15:32:00 -0400

    say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
 """
-Function that prints My name
+Functions that adds two integers.
+a and b must be integers or floats, otherwise raise a TypeError exception 
+a and b must be first casted to integers if they are float
 """
-
-
-def say_my_name(first_name, last_name=""):
+def matrix_divided(matrix, div):
     """
-    Function that prints My name
+    Functions that adds two integers.
+    a and b must be integers or floats, otherwise raise a TypeError exception 
+    a and b must be first casted to integers if they are float
     """
-    if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
-    if type(last_name) is not str:
-        raise TypeError('last_name must be a string')
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+    if not isinstance(matrix, list)and not isinstance(matrix[0], list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero") 
+    new_matrix = []
+    row_size = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_size:
+            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for element in row:
+            if isinstance(element, (int, float)):
+                new_row.append(round(element / div, 2))
+            else:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        new_matrix.append(new_row)
+    return new_matrix
