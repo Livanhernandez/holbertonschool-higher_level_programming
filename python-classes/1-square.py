7078dd9825c2a7bb2c34eccfb5a40f8746b0e23e
Livanhernandez <livanhernandez9@gmail.com>
2024-02-02 14:45:00 -0400

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This class defines a square and size
+"""
+
+
+class Square:
+    """ class Square defines a size
+    """
+    def __init__(self, size):
+        self.__size = size
