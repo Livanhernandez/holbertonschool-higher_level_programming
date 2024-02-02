01c9f0bfa2d1f40b4e016cad2d9f244c8fe744c9
Livanhernandez <livanhernandez9@gmail.com>
2024-02-02 14:48:47 -0400

    square

---
+++ python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This class defines a square and size
+"""
+
+
+class Square:
+    """ class - Square defines a size
+        defines area of square
+    """
+    def __init__(self, size=0):
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+# This will be the getter
+    @property
+    def size(self):
+        return self.__size
+
+# Its the setter
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
