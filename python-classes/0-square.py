5a1fd58289f4f10ea80f2380001431f4841eac27
Livanhernandez <livanhernandez9@gmail.com>
2024-02-02 14:42:23 -0400

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    try:
-        for i in range(0, x):
-            print('{}'.format(my_list[i]), end='')
-            count += 1
-    except IndexError:
-        pass
-    finally:
-        print()
-        return count
+""" My first class defines a square
+"""
+
+
+class Square:
+    """ class Square is an empty class
+ """
+    pass
