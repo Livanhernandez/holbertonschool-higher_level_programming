405e2adc4aa4b1cfadcb73e35518cba76d3f368c
Livanhernandez <livanhernandez9@gmail.com>
2024-02-08 21:59:45 -0400

    added commenta

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""This function returns all properties and methods of the specified object, without the values"""
+
 def lookup(obj):
+    """This function returns all properties and methods of the specified object, without the values"""
     return dir(obj)
