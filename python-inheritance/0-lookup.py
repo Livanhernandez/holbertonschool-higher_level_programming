64de98dde80d2d6657eaa10c82a57f2a44cbcca3
Livanhernandez <livanhernandez9@gmail.com>
2024-02-08 22:09:05 -0400

    added new line

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-"""This function returns all properties and methods of the specified object, without the values"""
+
+"""This function returns all properties
+ and methods of the specified object,
+   without the values"""
+
 
 def lookup(obj):
-    """This function returns all properties and methods of the specified object, without the values"""
+
+    """This function returns all properties
+      and methods of the specified object,
+        without the values"""
     return dir(obj)
