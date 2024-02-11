6b9d53a95fec9d51e096b8ec71e715dc9265f72b
Livanhernandez <livanhernandez9@gmail.com>
2024-02-11 19:37:18 -0400

    .

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""Module 11-student"""
+""" CLass returns specified atribute if it has them
+    it dictionary form
+"""
 
 
 class Student:
+    """ Student class will return __dict__
+    """
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        obj_dict = {}
         if attrs is None:
-            attrs = dir(self)
-        for i in attrs:
-            if i in dir(self):
-                obj_dict[i] = getattr(self, i)
-        return obj_dict
+            return self.__dict__
+        else:
+            has_atribute = {}
+            for atribute in attrs:
+                if hasattr(self, atribute):
+                    has_atribute[atribute] = getattr(self, atribute)
+        return has_atribute
 
     def reload_from_json(self, json):
         for key, value in json.items():
