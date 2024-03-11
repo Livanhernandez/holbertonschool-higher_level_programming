6c9d2fdfa325f93a84dbcdabcb38a1bf78ee672a
Livanhernandez <livanhernandez9@gmail.com>
2024-03-10 22:31:47 -0400

    fixed

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table second_table of the database in your MySQL server.
-SELECT * FROM second_table;
+SELECT score, name FROM second_table ORDER BY score DESC, name DESC;
