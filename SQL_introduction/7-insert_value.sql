e6009c812b6462fded28f5ca086db8ce1c8add33
Livanhernandez <livanhernandez9@gmail.com>
2024-03-10 22:18:43 -0400

    fixed

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,3 @@
 -- write script that inserts a new row in the table first_table in your MySQL server.
-INSERT INTO first_table (
-    id 89
-    name Best School
-);
+INSERT INTO first_table (id,name)
+VALUES ('89', 'Best School');
