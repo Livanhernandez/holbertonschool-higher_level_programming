d84f2a2b02f8649dbaa9ed031c61fe4ecbd06548
Livanhernandez <livanhernandez9@gmail.com>
2024-03-12 16:25:29 -0400

    fixed

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Provide a class that map to state table"""
-# Module for Connecting to MySQL database
+# Module for Connecting To MySQL database
+import sys
+from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,16 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
-    """class mapping table"""
-    __tablename__ = 'States'
-    id = Column(
-        Integer,
-        primary_key=True,
-        nullable=False,
-        autoincrement=True
-        )
-
-    name = Column(
-        String(128),
-        nullable=False
-        )
+    '''Class mapping table Sate'''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
