44aaa2661b087d4a96173deaefc0e57a0cf52038
Livanhernandez <livanhernandez9@gmail.com>
2024-03-12 16:57:14 -0400

    link class

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Start link class"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    db_api = 'mysql+mysqldb://{}:{}@localhost/{}'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    state = sys.argv[4]
+
+    engine = create_engine(db_api.format(
+        user,
+        password,
+        db),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like(state)).scalar()
+    if states is not None:
+        print(states.id)
+    else:
+        print("Not found")
