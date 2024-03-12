67e41802e314d974585c9f22b8878bba87a50165
Livanhernandez <livanhernandez9@gmail.com>
2024-03-12 14:40:38 -0400

    chmod

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that takes in an argument and display all values"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    db = connect(
+        host="localhost",
+        port=3306,
+        user=argv[1]
+        passwd=argv[2]
+        db=argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE '{}'\
+                   ORDER BY name ASC".format(argv[4]))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print("{}".format(row))
+
+    cursor.close()
+    db.close()
