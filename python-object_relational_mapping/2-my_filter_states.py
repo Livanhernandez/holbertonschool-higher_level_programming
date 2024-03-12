f77aee0fc99d62ba82204c10b4e241edf5a905bd
Livanhernandez <livanhernandez9@gmail.com>
2024-03-12 14:44:01 -0400

    fixed

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 """Script that takes in an argument and display all values"""
 
+import MySQLdb
 from sys import argv
-from MySQLdb import connect
 
 if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
 
-    db = connect(
-        host="localhost",
+    db = MySQLdb.connect(
+        host='localhost',
         port=3306,
-        user=argv[1]
-        passwd=argv[2]
-        db=argv[3]
-    )
+        user=username,
+        passwd=password,
+        db=db_name
+        )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE '{}'\
-                   ORDER BY name ASC".format(argv[4]))
-
+    query = "SELECT * FROM states WHERE\
+        BINARY name LIKE '{}'".format(state_name)
+    cursor.execute(query)
     rows = cursor.fetchall()
-
     for row in rows:
-        print("{}".format(row))
+        print(row)
 
     cursor.close()
     db.close()
